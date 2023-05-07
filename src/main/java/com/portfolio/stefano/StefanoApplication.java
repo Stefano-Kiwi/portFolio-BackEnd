ee3150054ac1e76473366da210a44ea054f7e4ba
Stefano-Kiwi <stefanobudon@gmail.com>
2023-05-07 19:54:08 -0300

    Commit inicial

---
+++ src/main/java/com/portfolio/stefano/StefanoApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.stefano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StefanoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StefanoApplication.class, args);
+	}
+
+}
