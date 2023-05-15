5a13c6db4d4c7ce271a9ee4ece9540a5fea1139e
Stefano-Kiwi <stefanobudon@gmail.com>
2023-05-15 00:53:16 -0300

    arreglo CORS

--- src/main/java/com/portfolio/stefano/Controller/CSkills.java
+++ src/main/java/com/portfolio/stefano/Controller/CSkills.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/skill")
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = {"https://stefano-frontend.web.app"})
 public class CSkills {
 
     @Autowired
